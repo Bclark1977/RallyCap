bd89da4dfe60219e76607466b555f292d5293bf2
Brian Clark <bclark@bclc.com>
2016-09-24 16:19:40 -0700

    Admin and Game day journey

---
+++ RallyCap/Journeys/JourneyHub.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading;
+using Microsoft.AspNet.SignalR;
+
+namespace RallyCap.Journeys
+{
+    public class JourneyHub : Hub
+    {
+        public static JourneyHub Instance;
+        public JourneyHub()
+        {
+            Instance = this;
+        }
+        public void Taunt(string messageType)
+        {
+            Clients.All.sendMessage(string.Format("PlayTaunt||{0}", messageType));
+            
+        }
+
+        private static bool GameStart = false;
+        private static int CountDownTimer = 50000;
+
+        private static bool isCountdownRunning = false;
+
+
+        private static System.Threading.Thread CountdownTimer_Thread;
+        private void CountdownTimer_OutOfProcess()
+        {
+            while (!GameStart && CountDownTimer > 0)
+            {
+                
+                Clients.All.sendMessage(string.Format("CountDown||{0}", CountDownTimer));
+                CountDownTimer--;
+                if (CountDownTimer <= 0)
+                {
+                    StartGameNow();
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        public void CountdownToGameTime()
+        {
+            if (isCountdownRunning)
+                return;
+            isCountdownRunning = true;
+            ThreadStart ths = new ThreadStart(CountdownTimer_OutOfProcess);
+            CountdownTimer_Thread = new Thread(ths);
+            CountdownTimer_Thread.Start();
+
+        }
+        public void StartGameNow()
+        {
+            isCountdownRunning = false;
+            CountDownTimer = 0;
+            GameStart = true;
+         
+            try
+            {
+                CountdownTimer_Thread.Abort();
+            }
+            catch
+            {
+                // swallow
+            }
+            Clients.All.sendMessage(string.Format("GameStart"));
+        }
+    }
+}
--- RallyCap/Startup.cs
+++ RallyCap/Startup.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
+
 [assembly: OwinStartupAttribute(typeof(RallyCap.Startup))]
 namespace RallyCap
 {
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
+            app.MapSignalR();
             ConfigureAuth(app);
+           
         }
     }
 }
