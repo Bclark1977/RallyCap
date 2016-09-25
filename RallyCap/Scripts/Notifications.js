66bb2e292568ae854965abeaa9ef797fc40f71ae
Brian Clark <bclark@bclc.com>
2016-09-25 13:55:19 -0700

    asdf

--- RallyCap/Scripts/Notifications.js
+++ RallyCap/Scripts/Notifications.js
@@ -31,7 +31,7 @@ function ShowContent(content) {
     var location = window.location + "";
 
     if (content.includes("CountDown")) {
-        document.getElementById("GameTime").style.display = "block";
+        var item = document.getElementById("GameTime"); if (item != null) item.style.display = "block";
         var seconds = content.split("||")[1];
         window.status = seconds;
         var location = window.location + "";
@@ -46,14 +46,14 @@ function ShowContent(content) {
         showMessageAtTopOfScreen("Game Starts in " + minutes + ":" + seconds)
     }
     else if (content == "GameStart") {
-        document.getElementById("GameTime").style.display = "block";
+        var item = document.getElementById("GameTime"); if (item != null) item.style.display = "block";
         if (!location.includes("Journeys/GameDay")) {
             window.location = "/Journeys/GameDay";
         }
     }
     else if (content.includes("Taunt")) {
         $.noty.closeAll();
-        document.getElementById("GameTime").style.display = "none";
+        var item = document.getElementById("GameTime"); if (item!=null) item.style.display = "none";
         $(".Taunt").each(function () {
             if (this.id == content.replace("||", "")) {
                 this.style.display = "block"
@@ -61,7 +61,7 @@ function ShowContent(content) {
         });
     }
     else if (content.includes("Chant")) {
-        document.getElementById("GameTime").style.display = "none";
+        var item = document.getElementById("GameTime"); if (item != null) item.style.display = "none";
         $(".Chant").each(function () {
             if (this.id == content.replace("||", "")) {
                 this.style.display = "block"
